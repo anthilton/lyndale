b2657d2ae363510a74e21b679cea4a741a5c0e0e
Anthony Hilton <anthilton@gmail.com>
2016-03-17 21:18:09 +0000

    update layout

--- js/main.js
+++ js/main.js
@@ -2,6 +2,152 @@ var offsetFromTop = 0;
 
 $(document).ready(function(e){
     
+    var modalValidator = $('#emailModalForm').parsley();
+    var modalValidator = $('#send-email-form').parsley();
+	var request;
+    $('#send-email-form').on("submit",function(event){
+		if(!modalValidator.isValid()){
+			return false;
+		}
+		
+		if (request) {
+			request.abort();
+		}
+		
+		var $form = $(this);
+	
+		// Let's select and cache all the fields
+		var $inputs = $form.find("input, select, button, textarea");
+	
+		// Serialize the data in the form
+		var serializedData = $form.serialize();
+		
+		$inputs.prop("disabled", true);
+		
+		$('#email-sending').removeClass("hide");
+					
+		var request = $.ajax({
+			type: 'POST',
+			url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+			data: {
+				'key': 'ovVqO3_aJHVaIpGZPHPn7A',
+				'message': {
+					'from_email': $('#email-address').val(),
+					'to': [
+						{
+							'email': 'anthilton@gmail.com',
+							'name': 'Anthony',
+							'type': 'to'
+						}
+					],
+				'autotext': 'true',
+				'subject': 'Lyndale Website Email',
+				'html': $('#modal-email-message').val()
+			}
+			}
+			});
+	
+	
+		// Callback handler that will be called on success
+		request.done(function (response, textStatus, jqXHR){
+			clearEmailFields();
+			$form.parsley().reset();
+			$('#email-sending').addClass("hide");
+			$('#email-sent').removeClass("hide");
+			setTimeout(function(){
+				$('#email-sent').addClass("hide");
+			}, 3000); 
+		});
+
+		// Callback handler that will be called on failure
+		request.fail(function (jqXHR, textStatus, errorThrown){
+			$('#email-sending').addClass("hide");
+			$('#email-sent').addClass("hide");
+		});
+		
+		// Callback handler that will be called regardless
+		// if the request failed or succeeded
+		request.always(function () {
+			// Reenable the inputs
+			$inputs.prop("disabled", false);
+		});
+		
+		event.preventDefault();
+		
+        
+    });
+	$('#emailModalForm').on("submit",function(event){
+		if(!modalValidator.isValid()){
+			return false;
+		}
+		
+		if (request) {
+			request.abort();
+		}
+		
+		var $form = $(this);
+	
+		// Let's select and cache all the fields
+		var $inputs = $form.find("input, select, button, textarea");
+	
+		// Serialize the data in the form
+		var serializedData = $form.serialize();
+		
+		$inputs.prop("disabled", true);
+		
+		$('#modal-email-sending').removeClass("hide");
+					
+		var request = $.ajax({
+			type: 'POST',
+			url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+			data: {
+				'key': 'ovVqO3_aJHVaIpGZPHPn7A',
+				'message': {
+					'from_email': $('#modal-email-address').val(),
+					'to': [
+						{
+							'email': 'anthilton@gmail.com',
+							'name': 'Anthony',
+							'type': 'to'
+						}
+					],
+				'autotext': 'true',
+				'subject': 'Lyndale Website Email',
+				'html': $('#modal-email-message').val()
+			}
+			}
+			});
+	
+	
+		// Callback handler that will be called on success
+		request.done(function (response, textStatus, jqXHR){
+			clearModalEmailFields();
+			$form.parsley().reset();
+			$('#modal-email-sending').addClass("hide");
+			$('#modal-email-sent').removeClass("hide");
+			setTimeout(function(){
+				$('#modal-email-sent').addClass("hide");
+			}, 3000); 
+		});
+
+		// Callback handler that will be called on failure
+		request.fail(function (jqXHR, textStatus, errorThrown){
+			$('#modal-email-sending').addClass("hide");
+			$('#modal-email-sent').addClass("hide");
+		});
+		
+		// Callback handler that will be called regardless
+		// if the request failed or succeeded
+		request.always(function () {
+			// Reenable the inputs
+			$inputs.prop("disabled", false);
+		});
+		
+		event.preventDefault();
+		
+        
+    });
+    
     var fontTargets = ['h1','h2','h3','h4','h5','p'];
     var arrayLength = fontTargets.length;
     $('.text-increase .decrease').on("click",function(){
@@ -59,3 +205,19 @@ $(document).ready(function(e){
         }
     });
 });
+
+
+function clearModalEmailFields(){
+	$("#modal-email-name").val("");
+	$("#modal-email-address").val("");
+	$("#modal-email-telephone").val("");
+	$("#modal-email-message").val("");
+};
+
+
+function clearEmailFields(){
+	$("#email-name").val("");
+	$("#email-address").val("");
+	$("#email-telephone").val("");
+	$("#email-message").val("");
+};
